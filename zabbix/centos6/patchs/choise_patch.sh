4983f6b37b68008e32522067f84450969f5e8fc0
boringcat <boringcat@outlook.com>
2024-06-25 13:56:29 +0800

    修复zabbix问题 - java-gateway 启动问题 - systemd依赖问题

--- zabbix/centos6/patchs/choise_patch.sh
+++ zabbix/centos6/patchs/choise_patch.sh
@@ -7,9 +7,11 @@ case "${zabbix_version}" in
     "5.0")
         patch -d ~/rpmbuild/SPECS/ -p1 -i ~/patchs/withpgsql-5.0.patch
         patch -d ~/rpmbuild/SPECS/ -p1 -i ~/patchs/zabbix-spec-5.0.patch
+        patch -d ~/rpmbuild/SOURCES/ -p1 -i ~/patchs/zabbix-java-gateway-init-5.0.patch
     ;;
     "6.0")
         patch -d ~/rpmbuild/SPECS/ -p1 -i ~/patchs/withpgsql-6.0.patch
         patch -d ~/rpmbuild/SPECS/ -p1 -i ~/patchs/zabbix-spec-6.0.patch
+        patch -d ~/rpmbuild/SOURCES/ -p1 -i ~/patchs/zabbix-java-gateway-init-6.0.patch
     ;;
 esac
